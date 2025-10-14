a2d514489370978762fa16447ac3fa687ff680c5
Timofei-Gomziakov-TU <tuk36968@temple.edu>
2025-10-13 21:32:02 -0400

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* Pokemon List */}
+      {/* Filtering System based */}
+      {/* Table */}
+    </>
+  );
+}
+
+export default App;
