055f2dc5cd73e976798d4e578fa89ed8ca8322f7
Timofei-Gomziakov-TU <tuk36968@temple.edu>
2025-12-28 22:04:27 -0500

    Changed the naming to better reflect the rest of the naming conventions

---
+++ src/data/moveGroups.ts
@@ -0,0 +1,25 @@
+/**Moves that switch the user out of the field */
+export const switchOutMoves = {
+  groupName: "Pivots",
+  moves: [
+    "Baton Pass",
+    "U-turn",
+    "Volt Switch",
+    "Flip Turn",
+    "Parting Shot",
+    "Teleport",
+    "Shed Tail",
+  ],
+};
+
+/**Moves that setup entry hazards */
+export const entryHazards = {
+  groupName: "Entry Hazards",
+  moves: ["Stealth Rock", "Spikes", "Toxic Spikes", "Sticky Web"],
+};
+
+/**Moves that remove entry hazards */
+export const entryHazardsRemoval = {
+  groupName: "Hazard Removals",
+  moves: ["Rapid Spin", "Mortal Spin", "Defog", "Tidy Up", "Court Change"],
+};
